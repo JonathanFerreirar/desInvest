dca77ac02a5313ad682cbae5c0f6edebae683745
jhowf <jonathanferreirar@gmail.com>
2023-07-06 23:42:23 -0300

    :sparkles: feat: create home.js page and add a simples form to same

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
-export const Button = () => {
+export const Button = ({ children }) => {
   return (
     <div>
-      <button className="bg-[#FF7C04] py-[15px] px-[18px] text-[#FFF] rounded-[5px]">Botão</button>
+      <button className="bg-[#FF7C04] py-[15px] px-[18px] text-[#FFF] rounded-[5px]">
+        {children}
+      </button>
     </div>
   );
 };
