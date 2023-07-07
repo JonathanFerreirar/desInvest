caddac2d3679c6809b240c353bc34f8e6658bae8
Alice De França Vidal <alicefrancavidal@gmail.com>
2023-07-06 21:46:51 -0300

    :sparkes: feat: new component input

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { App } from "./App";
+import { Input } from "./components/input/Input";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/input" element={<Input />} />
         </Routes>
       </BrowserRouter>
     </div>
