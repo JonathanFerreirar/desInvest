f4e461dc8d897a13df1129b54a0ed6cd39efb1f7
jhowf <jonathanferreirar@gmail.com>
2023-07-20 01:11:56 -0300

    :lipstick: feat: finish reusable button component

--- src/components/button/button.style.js
+++ src/components/button/button.style.js
@@ -1,6 +1,6 @@
 export const buttonStyle = {
   regularClass:
-    "flex items-center px-3 text-center py-1.5 border h-[37px] font-default font-button rounded-default flex justify-center items-center",
+    "px-3 text-center py-1.5 border h-[37px] font-default font-button rounded-default flex justify-center items-center",
   primaryClass: "text-white bg-button  hover:bg-white hover:text-button",
   outlineClass: "bg-white text-button hover:bg-button hover:text-white",
 };
