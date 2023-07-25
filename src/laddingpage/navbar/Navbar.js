f21590d91b357b83f77edd4064cabfc59cd1f353
Alice De França Vidal <alicefrancavidal@gmail.com>
2023-07-25 17:11:00 -0300

    :sparkless: feat: Navbar prototype

--- src/laddingpage/navbar/Navbar.js
+++ src/laddingpage/navbar/Navbar.js
@@ -1,5 +1,24 @@
+import img from "../logo/logo-oficial.svg";
 const Navbar = () => {
-  return <div>Navbra</div>;
+  return (
+    <div className="m-0 box-border p-0">
+      <header className=" flex flex-wrap items-center justify-between">
+        <img src={img} />
+        <nav>
+          <ul>
+            <li className="px-15 inline-block py-5">
+              <a href="#">Funcionalidades</a>
+            </li>
+            <li className="px-15 inline-block py-5">
+              <a href="#">Simulador</a>
+            </li>
+            <li className="px-15 inline-block py-5">
+              <a href="#">Planos</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 };
-
 export { Navbar };
